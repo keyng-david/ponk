048b011ec9017e68c1f2a68acd400b4a73faf00c
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-11 20:13:37 +0100

    Update index.ts

--- frontend/src/entities/leaders/model/index.ts
+++ frontend/src/entities/leaders/model/index.ts
@@ -1,50 +1,47 @@
 import {combine, createEffect, createEvent, createStore, sample} from "effector";
-
-import { LeaderData } from './types'
+import { LeaderData } from './types';
 import { leadersApi } from "@/shared/api/leaders";
-import {ResponseDefault} from "@/shared/lib/api/createRequest";
-import {GetLeaderListResponse} from "@/shared/api/leaders/types";
+import { GetLeaderListResponse } from "@/shared/api/leaders/types";
 
-const fetchFx = createEffect(leadersApi.getList)
+const fetchFx = createEffect(leadersApi.getList);
 
-const leadersRequested = createEvent()
+const leadersRequested = createEvent();
 const $data = createStore<LeaderData[]>([{
     position: 1,
     name: '',
     score: 1,
-}])
-const $firstPosition = combine($data, list => list[0])
-const $list = combine($data, list => list.slice(1))
-const $isLoading = fetchFx.pending
+}]);
+
+const $firstPosition = combine($data, list => list[0]);
+const $list = combine($data, list => list.slice(1));
+const $isLoading = fetchFx.pending;
 
 sample({
     clock: leadersRequested,
     target: fetchFx,
-})
+});
 
 sample({
     clock: fetchFx.doneData,
-    fn: leadersToDomain,
+    fn: leadersToDomain, // Modified to handle GetLeaderListResponse type
     target: $data,
-})
+});
 
 export const leadersModel = {
     $list,
     $firstPosition,
-
     $isLoading,
-
     leadersRequested,
-}
+};
 
-function leadersToDomain(data: ResponseDefault<GetLeaderListResponse>): LeaderData[] {
+// Adjust the function to expect GetLeaderListResponse
+function leadersToDomain(data: GetLeaderListResponse): LeaderData[] {
     if (data.payload) {
         return data.payload.leaders.map((item, key) => ({
             position: key + 1,
             name: item.username,
             score: item.score,
-        }))
+        }));
     }
-
-    return []
+    return [];
 }
