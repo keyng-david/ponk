4fe8a695f5d5f8725c6c65ccbff1f42cdee229e3
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-11 16:36:40 +0100

    Update rest.ts

--- frontend/src/shared/api/leaders/rest.ts
+++ frontend/src/shared/api/leaders/rest.ts
@@ -1,9 +1,16 @@
-import {LeadersApi} from "@/shared/api/leaders/types";
-import {createRequest} from "@/shared/lib/api/createRequest";
+import { LeadersApi } from './types';
+import { $sessionId } from "@/shared/model/session";
 
 export const leadersApi: LeadersApi = {
-    getList: async () => await createRequest({
-        endpoint: 'game/leaders',
-        method: 'GET',
-    })
-}
+  getList: async () => {
+    const sessionId = $sessionId.getState();
+    const response = await fetch(`/game/leaders`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${sessionId}`,
+      },
+    });
+    return response.json();
+  },
+};
