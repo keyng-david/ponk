95e1b73c07bf73e61ccdeea2f87846d815a7f592
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-11 16:37:02 +0100

    Update types.ts

--- frontend/src/shared/api/leaders/types.ts
+++ frontend/src/shared/api/leaders/types.ts
@@ -1,12 +1,13 @@
-import {ResponseDefault} from "@/shared/lib/api/createRequest";
-
 export type GetLeaderListResponse = {
+  error: boolean;
+  payload: {
     leaders: {
-        username: string
-        score: number
-    }[]
-}
+      username: string;
+      score: number;
+    }[];
+  } | null;
+};
 
 export type LeadersApi = {
-    getList: () => Promise<ResponseDefault<GetLeaderListResponse>>
-}
+  getList: () => Promise<GetLeaderListResponse>; // Adjusted to match native fetch response
+};
