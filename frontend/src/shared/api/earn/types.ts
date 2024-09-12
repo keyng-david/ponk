1f2b2e6319b713306a2f2e1a0afed758abcb9af9
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-12 09:32:21 +0100

    Update types.ts

--- frontend/src/shared/api/earn/types.ts
+++ frontend/src/shared/api/earn/types.ts
@@ -1,33 +1,28 @@
 export type GetEarnDataResponseItem = {
-    name: string,
-    description: string,
-    reward: string,
-    reward1: string,
-    reward2: string,
-    reward3: string,
-    reward_symbol: string,
-    end_time: number,
-    id: number,
-    total_clicks: number,
-    link: string,
-    image_link: string,
-    task_list: string[]
-};
+    name: string;
+    description: string;
+    reward: string;
+    reward1: string;
+    reward2: string;
+    reward3: string;
+    reward_symbol: string;
+    end_time: number;
+    id: number;
+    total_clicks: number;
+    link: string;
+    image_link: string;
+    task_list: string[];
+}
 
 export type GetEarnDataResponse = {
     error: boolean;
-    message?: string;
     payload?: {
-        tasks: GetEarnDataResponseItem[],
-        user_level: number
+        tasks: GetEarnDataResponseItem[];
+        user_level: number;
     };
-};
+}
 
 export type EarnApi = {
     getData: () => Promise<GetEarnDataResponse>;
-    taskJoined: (data: { id: number }) => Promise<{ 
-        error: boolean, 
-        message: string,
-        payload?: any // Adding payload as optional for flexibility
-    }>;
-};
+    taskJoined: (data: { id: number }) => Promise<{ error: boolean; message: string }>;
+}
