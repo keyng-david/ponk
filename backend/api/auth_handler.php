8dbeff9da0cbab67c7786d20bdab9207bb5f10a2
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-14 12:19:57 +0100

    Update auth_handler.php

--- backend/api/auth_handler.php
+++ backend/api/auth_handler.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $mysqli = getDbConnection();
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-// Ensure request is POST
+// Ensure request is post
 if ($requestMethod !== 'POST') {
     header('Content-Type: application/json');
     header('HTTP/1.0 405 Method Not Allowed');
