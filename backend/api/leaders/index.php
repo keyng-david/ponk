a4afd524ec22702e120fb594984b6a337649dda6
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-29 13:43:49 +0100

    Update index.php

--- backend/api/leaders/index.php
+++ backend/api/leaders/index.php
@@ -3,12 +3,12 @@ require __DIR__ . '/../db_connection.php';
 
 $mysqli = getDbConnection();
 
-$result = $mysqli->query("SELECT username, score FROM users ORDER BY score DESC LIMIT 10");
+$result = $mysqli->query("SELECT telegram_id, score FROM users ORDER BY score DESC LIMIT 10");
 
 if ($result->num_rows > 0) {
     $leaders = [];
     while ($row = $result->fetch_assoc()) {
-        $leaders[] = ['username' => $row['username'], 'score' => $row['score']];
+        $leaders[] = ['username' => $row['telegram_id'], 'score' => $row['score']];
     }
     echo json_encode(['error' => false, 'payload' => ['leaders' => $leaders]]);
 } else {
