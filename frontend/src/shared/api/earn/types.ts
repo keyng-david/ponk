8be3a298dd888744127b35d4eb8406a96fd64bfb
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-09-30 12:03:13 +0100

    Update types.ts

--- frontend/src/shared/api/earn/types.ts
+++ frontend/src/shared/api/earn/types.ts
@@ -34,5 +34,5 @@ export type GetEarnDataResponse = ResponseDefault<{
 export type EarnApi = {
   getData: () => Promise<GetEarnDataResponse>;
   getUserTasks: () => Promise<{ task_id: number; status: string }[]>;  // Already added this line
-  taskJoined: (data: { id: number; reward: number }) => Promise<ResponseDefault<any>>;  // Include reward
+  taskJoined: (data: { id: number; reward: string }) => Promise<ResponseDefault<any>>;  // Include reward
 };
