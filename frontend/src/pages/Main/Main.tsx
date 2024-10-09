f57348d5358e2c807e000df5fba83853bd9b53fd
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-09 06:35:51 +0100

    Update Main.tsx

--- frontend/src/pages/Main/Main.tsx
+++ frontend/src/pages/Main/Main.tsx
@@ -1,11 +1,3 @@
-import backgroundImage from '@/shared/assets/images/main/background.svg'
-import tableImage from '@/shared/assets/images/main/table.svg'
-import cabelImage from '@/shared/assets/images/main/cabel.svg'
-import clockImage from '@/shared/assets/images/main/clock.png'
-import pictureImage from '@/shared/assets/images/main/picture.png'
-import keyboardImage from '@/shared/assets/images/main/keyboard.svg'
-import bankaLeftImage from '@/shared/assets/images/main/banka-left.png'
-import bankaRightImage from '@/shared/assets/images/main/banka-right.png'
 import logo from '@/shared/assets/images/main/logo.png'
 import walletButton from '@/shared/assets/images/main/wallet-button.png'
 
@@ -15,10 +7,6 @@ import lieutenant from '@/shared/assets/images/rangs/lieutnent.png'
 import general from '@/shared/assets/images/rangs/general.png'
 
 import styles from './Main.module.scss'
-import {MonitorTop} from "@/shared/ui/MonitorTop";
-import {MonitorCenter} from "@/shared/ui/MonitorCenter";
-import {MonitorRight} from "@/shared/ui/MonitorRight";
-import {MonitorLeft} from "@/shared/ui/MonitorLeft";
 import {ClickerField} from "@/features/clicker/ui";
 import {useConnectTon} from "@/features/ton/useConnectTon";
 import {useAuth} from "@/features/auth/useAuth";
@@ -76,50 +64,6 @@ export const Main = () => {
 }
 
 const Background = () => (
-    <div className={styles.background}>
-            <img
-                className={styles['background-image']}
-                src={backgroundImage}
-                alt={'background'}
-            />
-            <img
-                className={styles.table}
-                src={tableImage}
-                alt={'table'}
-            />
-            <img
-                className={styles.cabel}
-                src={cabelImage}
-                alt={'cabel'}
-            />
-            <img
-                className={styles.clock}
-                src={clockImage}
-                alt={'clock'}
-            />
-            <img
-                className={styles.picture}
-                src={pictureImage}
-                alt={'picture'}
-            />
-            <img
-                className={styles.keyboard}
-                src={keyboardImage}
-                alt={'keyboard'}
-            />
-            <img
-                className={styles['banka-left']}
-                src={bankaLeftImage}
-                alt={'banka-left'}
-            />
-            <img
-                className={styles['banka-right']}
-                src={bankaRightImage}
-                alt={'banka-right'}
-            />
-            <MonitorTop className={styles['monitor-top']}/>
-            <MonitorCenter className={styles['monitor-center']}/>
-            <MonitorRight className={styles['monitor-right']}/>
-            <MonitorLeft className={styles['monitor-left']}/>
+    <div className={styles.background} style={{ backgroundColor: '#95866C' }}>
     </div>
 )
