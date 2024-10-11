07c2ab6074a7c9c9b2301b12d0b0365e479c784d
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-11 14:38:33 +0100

    Update NavBar.tsx

--- frontend/src/shared/ui/NavBar/NavBar.tsx
+++ frontend/src/shared/ui/NavBar/NavBar.tsx
@@ -1,62 +1,74 @@
-import home from '@/shared/assets/images/navbar/home.png'
-import frens from '@/shared/assets/images/navbar/frens.png'
-import board from '@/shared/assets/images/navbar/board.png'
-import earn from '@/shared/assets/images/navbar/earn.png'
-
-import styles from './NavBar.module.scss'
-import React, {useCallback} from "react";
-import {Steps, useNavigatorModel} from "@/shared/model";
-import {leadersModel} from "@/entities/leaders/model";
-import {earnModel} from "@/entities/earn/model";
+import React from 'react';
+import { useNavigatorModel, Steps } from '@/shared/model';
+import { leadersModel } from '@/entities/leaders/model';
+import { earnModel } from '@/entities/earn/model';
 
 export const NavBar = () => {
-    const { step, stepChanged } = useNavigatorModel()
+  const { step, stepChanged } = useNavigatorModel();
 
-    const getClasses = useCallback((page: Steps) => {
-        const classes = [
-            styles.item,
-            styles[`item-${page}`],
-        ]
+  return (
+    <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+      <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
+        <button
+          type="button"
+          className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          onClick={() => stepChanged(Steps.HOME)}
+        >
+          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+          </svg>
+          <span className="sr-only">Home</span>
+        </button>
 
-        if (page === step) {
-            classes.push(styles['is-active'])
-        }
+        <button
+          type="button"
+          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          onClick={() => stepChanged(Steps.FRENS)}
+        >
+          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"/>
+          </svg>
+          <span className="sr-only">Frens</span>
+        </button>
 
-        return classes.join(' ')
-    }, [step])
-
-    return (
-        <div className={styles.root}>
-            <img
-                className={getClasses(Steps.HOME)}
-                src={home}
-                alt={'main'}
-                onClick={() => stepChanged(Steps.HOME)}
-            />
-            <img
-                className={getClasses(Steps.FRENS)}
-                src={frens}
-                alt={'frens'}
-                onClick={() => stepChanged(Steps.FRENS)}
-            />
-            <img
-                className={getClasses(Steps.BOARD)}
-                src={board}
-                alt={'board'}
-                onClick={() => {
-                    leadersModel.leadersRequested()
-                    stepChanged(Steps.BOARD)
-                }}
-            />
-            <img
-                className={getClasses(Steps.EARN)}
-                src={earn}
-                alt={'earn'}
-                onClick={() => {
-                    earnModel.tasksRequested()
-                    stepChanged(Steps.EARN)
-                }}
-            />
+        <div className="flex items-center justify-center">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group"
+            onClick={() => stepChanged(Steps.EARN)}
+          >
+            <svg className="w-4 h-4 text-white" aria-hidden="true" fill="none" viewBox="0 0 18 18">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
+            </svg>
+            <span className="sr-only">Earn</span>
+          </button>
         </div>
-    )
-}
+
+        <button
+          type="button"
+          className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          onClick={() => {
+            leadersModel.leadersRequested();
+            stepChanged(Steps.BOARD);
+          }}
+        >
+          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+          </svg>
+          <span className="sr-only">Leaderboard</span>
+        </button>
+
+        <button
+          type="button"
+          className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group"
+          onClick={() => alert('Coming Soon!')}
+        >
+          <svg className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
+          </svg>
+          <span className="sr-only">Profile</span>
+        </button>
+      </div>
+    </div>
+  );
+};
