3022b8e98a35e35236600e25acca51681d518ba3
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-16 18:34:06 +0100

    Update Navigator.tsx

--- frontend/src/shared/ui/Navigator/Navigator.tsx
+++ frontend/src/shared/ui/Navigator/Navigator.tsx
@@ -1,62 +1,80 @@
-import React, { useCallback } from "react";
-import { Steps, useNavigatorModel } from "@/shared/model";
-import { IonIcon } from 'react-ionicons';
-import styles from './NavBar.module.scss';
-import {leadersModel} from "@/entities/leaders/model";
-import {earnModel} from "@/entities/earn/model";
+import React, {useCallback, useEffect, useState} from "react";
 
-export const NavBar = () => {
-    const { step, stepChanged } = useNavigatorModel();
+import styles from './Navigator.module.scss'
+import {NavBar} from "@/shared/ui/NavBar";
+import { clickerModel } from "@/features/clicker/model";
 
-    const handleItemClick = (page: Steps) => {
-        stepChanged(page);
-    };
+export type NavigatorProps = {
+    activeStep: number
+    mainComponent: React.ReactNode
+    hiddenComponents: {
+        step: number
+        component: React.ReactNode
+    }[]
+}
 
-    const handleDropClick = () => {
-        alert("Coming Soon");
-    };
+export const SliderNavigator = React.memo<NavigatorProps>(({ activeStep, mainComponent, hiddenComponents }) => {
+    const [ active, setActive ] = useState(activeStep)
+    const [ prev, setPrev ] = useState<number[]>([])
+    const [ next, setNext ] = useState(0)
+
+    const { isMultiError } = clickerModel.useClicker()
+
+    const getClasses = useCallback((step: number) => {
+        const classes = [styles.step]
+
+        if (prev.includes(step)) {
+            classes.push(styles['step-prev'])
+        }
+
+        if (step === active) {
+            classes.push(styles['step-active'])
+        }
+
+        if (step === next) {
+            classes.push(styles['step-next'])
+        }
+
+        return classes.join(' ')
+    }, [active, prev, next])
+
+    useEffect(() => {
+        if (active !== activeStep) {
+            setNext(activeStep)
+            const timeout1 = setTimeout(() => {
+                setPrev(prevState => [
+                    ...prevState.filter(item => item !== activeStep),
+                    active,
+                ])
+                setActive(activeStep)
+                setNext(0)
+
+                clearTimeout(timeout1)
+            }, 300)
+        }
+    }, [activeStep, active, prev]);
 
     return (
-        <div className="navigation">
-            <ul>
-                <li className={`list_item ${step === Steps.HOME ? 'active' : ''}`} onClick={() => handleItemClick(Steps.HOME)}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="home-outline" /></span>
-                        <span className="text">Home</span>
-                    </a>
-                </li>
-                <li className={`list_item ${step === Steps.BOARD ? 'active' : ''}`} onClick={() => {
-                    leadersModel.leadersRequested()
-                    stepChanged(Steps.BOARD)
-                }}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="trophy-outline" /></span>
-                        <span className="text">Leaders</span>
-                    </a>
-                </li>
-                <li className={`list_item ${step === Steps.FRENS ? 'active' : ''}`} onClick={() => handleItemClick(Steps.FRENS)}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="people-outline" /></span>
-                        <span className="text">Frens</span>
-                    </a>
-                </li>
-                <li className={`list_item ${step === Steps.EARN ? 'active' : ''}`} onClick={() => {
-                    earnModel.tasksRequested()
-                    stepChanged(Steps.EARN)
-                }}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="cash-outline" /></span>
-                        <span className="text">Earn</span>
-                    </a>
-                </li>
-                <li className="list_item" onClick={handleDropClick}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="gift-outline" /></span>
-                        <span className="text">Airdrop</span>
-                    </a>
-                </li>
-                <div className="indicator"></div>
-            </ul>
+        <div className={styles.root}>
+            {mainComponent}
+            {hiddenComponents.map(({ step, component }) => {
+                if (step === active || prev.includes(step) || step === next) {
+                    return (
+                        <div key={step} className={getClasses(step)}>
+                            {component}
+                        </div>
+                    )
+                }
+
+                return null
+            })}
+            <NavBar />
+            {isMultiError && (
+                <div className={styles['invalid-platform']}>
+                    <h1>YOUR CAN USE ONLY ONE DEVICE</h1>
+                    <h1>YOUR CAN USE ONLY ONE DEVICE</h1>
+                </div>
+            )}
         </div>
-    );
-};
+    )
+})
