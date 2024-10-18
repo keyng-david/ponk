fd39ce9a7ff6c38ca23e3c028d1933408543834f
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-18 11:19:52 +0100

    Update NavBar.tsx

--- frontend/src/shared/ui/NavBar/NavBar.tsx
+++ frontend/src/shared/ui/NavBar/NavBar.tsx
@@ -1,9 +1,8 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { Steps, useNavigatorModel } from "@/shared/model";
-import { IonIcon } from '@ionic/react';
 import styles from './NavBar.module.scss';
-import {leadersModel} from "@/entities/leaders/model";
-import {earnModel} from "@/entities/earn/model";
+import { leadersModel } from "@/entities/leaders/model";
+import { earnModel } from "@/entities/earn/model";
 
 export const NavBar = () => {
     const { step, stepChanged } = useNavigatorModel();
@@ -20,38 +19,38 @@ export const NavBar = () => {
         <div className="navigation">
             <ul>
                 <li className={`list_item ${step === Steps.HOME ? 'active' : ''}`} onClick={() => handleItemClick(Steps.HOME)}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="home-outline" /></span>
+                    <a href="#">
+                        <span className="icon"><ion-icon name="home-outline"></ion-icon></span>
                         <span className="text">Home</span>
                     </a>
                 </li>
                 <li className={`list_item ${step === Steps.BOARD ? 'active' : ''}`} onClick={() => {
-                    leadersModel.leadersRequested()
-                    stepChanged(Steps.BOARD)
+                    leadersModel.leadersRequested();
+                    stepChanged(Steps.BOARD);
                 }}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="trophy-outline" /></span>
+                    <a href="#">
+                        <span className="icon"><ion-icon name="trophy-outline"></ion-icon></span>
                         <span className="text">Leaders</span>
                     </a>
                 </li>
                 <li className={`list_item ${step === Steps.FRENS ? 'active' : ''}`} onClick={() => handleItemClick(Steps.FRENS)}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="people-outline" /></span>
+                    <a href="#">
+                        <span className="icon"><ion-icon name="people-outline"></ion-icon></span>
                         <span className="text">Frens</span>
                     </a>
                 </li>
                 <li className={`list_item ${step === Steps.EARN ? 'active' : ''}`} onClick={() => {
-                    earnModel.tasksRequested()
-                    stepChanged(Steps.EARN)
+                    earnModel.tasksRequested();
+                    stepChanged(Steps.EARN);
                 }}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="cash-outline" /></span>
+                    <a href="#">
+                        <span className="icon"><ion-icon name="cash-outline"></ion-icon></span>
                         <span className="text">Earn</span>
                     </a>
                 </li>
                 <li className="list_item" onClick={handleDropClick}>
-                    <a href="#" style={{ color: 'white' }}>
-                        <span className="icon"><IonIcon name="gift-outline" /></span>
+                    <a href="#">
+                        <span className="icon"><ion-icon name="gift-outline"></ion-icon></span>
                         <span className="text">Airdrop</span>
                     </a>
                 </li>
