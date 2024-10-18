22ec6a20cc0de6a2d40735e2bb3cf9b9f80a6cac
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-18 14:18:41 +0100

    Update NavBar.tsx

--- frontend/src/shared/ui/NavBar/NavBar.tsx
+++ frontend/src/shared/ui/NavBar/NavBar.tsx
@@ -3,6 +3,8 @@ import { Steps, useNavigatorModel } from "@/shared/model";
 import styles from './NavBar.module.scss';
 import { leadersModel } from "@/entities/leaders/model";
 import { earnModel } from "@/entities/earn/model";
+import { IonIcon } from '@ionic/react';
+import { homeOutline, trophyOutline, peopleOutline, cashOutline, giftOutline } from 'ionicons/icons';
 
 export const NavBar = () => {
     const { step, stepChanged } = useNavigatorModel();
@@ -20,7 +22,7 @@ export const NavBar = () => {
             <ul>
                 <li className={`list_item ${step === Steps.HOME ? 'active' : ''}`} onClick={() => handleItemClick(Steps.HOME)}>
                     <a href="#">
-                        <span className="icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span className="icon"><IonIcon icon={homeOutline} /></span>
                         <span className="text">Home</span>
                     </a>
                 </li>
@@ -29,13 +31,13 @@ export const NavBar = () => {
                     stepChanged(Steps.BOARD);
                 }}>
                     <a href="#">
-                        <span className="icon"><ion-icon name="trophy-outline"></ion-icon></span>
+                        <span className="icon"><IonIcon icon={trophyOutline} /></span>
                         <span className="text">Leaders</span>
                     </a>
                 </li>
                 <li className={`list_item ${step === Steps.FRENS ? 'active' : ''}`} onClick={() => handleItemClick(Steps.FRENS)}>
                     <a href="#">
-                        <span className="icon"><ion-icon name="people-outline"></ion-icon></span>
+                        <span className="icon"><IonIcon icon={peopleOutline} /></span>
                         <span className="text">Frens</span>
                     </a>
                 </li>
@@ -44,13 +46,13 @@ export const NavBar = () => {
                     stepChanged(Steps.EARN);
                 }}>
                     <a href="#">
-                        <span className="icon"><ion-icon name="cash-outline"></ion-icon></span>
+                        <span className="icon"><IonIcon icon={cashOutline} /></span>
                         <span className="text">Earn</span>
                     </a>
                 </li>
                 <li className="list_item" onClick={handleDropClick}>
                     <a href="#">
-                        <span className="icon"><ion-icon name="gift-outline"></ion-icon></span>
+                        <span className="icon"><IonIcon icon={giftOutline} /></span>
                         <span className="text">Airdrop</span>
                     </a>
                 </li>
