cee59123d8514abb11d486fb80fe2ca139c8a4a1
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-21 04:59:26 +0100

    Update index.ts

--- frontend/src/features/clicker/model/index.ts
+++ frontend/src/features/clicker/model/index.ts
@@ -3,7 +3,11 @@ import { useUnit } from "effector-react";
 import { usePoints } from "@/app/socketProvider";
 import { useAuth } from "@/features/auth/useAuth";
 
+
 const { clickStep } = useAuth();
+
+export const MAX_AVAILABLE = useUnit($available);
+export const CLICK_STEP = useUnit(clickStep);
 
 const valueInited = createEvent<number>();
 const availableInited = createEvent<number>();
@@ -21,19 +25,6 @@ const $value = createStore(0).on(valueInited, (_, payload) => payload);
 const $available = createStore(MAX_AVAILABLE).on(availableInited, (_, payload) => payload);
 
 const $earnedPoint = createStore(0); // New store for accumulated points
-
-
-export let MAX_AVAILABLE = $available.getState();
-export let CLICK_STEP = clickStep.getState();
-
-// Update MAX_AVAILABLE and CLICK_STEP dynamically
-$available.watch((newAvailable) => {
-    MAX_AVAILABLE = newAvailable;
-});
-
-clickStep.watch((newClickStep) => {
-    CLICK_STEP = newClickStep;
-});
 
 const $canBeClicked = $available.map(state => state >= CLICK_STEP);
 
