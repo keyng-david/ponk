64681c55efa0a242b95a4eb20e21c3572316b26f
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-10-23 13:43:53 +0100

    Update index.ts

--- frontend/src/entities/leaders/model/index.ts
+++ frontend/src/entities/leaders/model/index.ts
@@ -1,47 +1,54 @@
-import {combine, createEffect, createEvent, createStore, sample} from "effector";
+import { createEffect, createEvent, createStore, sample } from 'effector';
 import { LeaderData } from './types';
-import { leadersApi } from "@/shared/api/leaders";
-import { GetLeaderListResponse } from "@/shared/api/leaders/types";
+import { leadersApi } from '@/shared/api/leaders';
+import { GetLeaderListResponse } from '@/shared/api/leaders/types';
 
 const fetchFx = createEffect(leadersApi.getList);
 
 const leadersRequested = createEvent();
-const $data = createStore<LeaderData[]>([{
-    position: 1,
-    name: '',
-    score: 1,
-}]);
-
-const $firstPosition = combine($data, list => list[0]);
-const $list = combine($data, list => list.slice(1));
+const $data = createStore<LeaderData[]>([]);
+const $userLeaderData = createStore<LeaderData | null>(null);
 const $isLoading = fetchFx.pending;
 
 sample({
-    clock: leadersRequested,
-    target: fetchFx,
+  clock: leadersRequested,
+  target: fetchFx,
 });
 
 sample({
-    clock: fetchFx.doneData,
-    fn: leadersToDomain, // Modified to handle GetLeaderListResponse type
-    target: $data,
+  clock: fetchFx.doneData,
+  fn: data => {
+    if (data.payload) {
+      const leaders = data.payload.leaders.map((item, index) => ({
+        position: index + 1,
+        name: item.username,
+        score: item.score,
+      }));
+      return leaders;
+    }
+    return [];
+  },
+  target: $data,
+});
+
+sample({
+  clock: fetchFx.doneData,
+  fn: data => {
+    if (data.payload && data.payload.userLeaderData) {
+      return {
+        position: data.payload.userLeaderData.position,
+        name: data.payload.userLeaderData.username,
+        score: data.payload.userLeaderData.score,
+      };
+    }
+    return null;
+  },
+  target: $userLeaderData,
 });
 
 export const leadersModel = {
-    $list,
-    $firstPosition,
-    $isLoading,
-    leadersRequested,
+  $list: $data,
+  $userLeaderData,
+  $isLoading,
+  leadersRequested,
 };
-
-// Adjust the function to expect GetLeaderListResponse
-function leadersToDomain(data: GetLeaderListResponse): LeaderData[] {
-    if (data.payload) {
-        return data.payload.leaders.map((item, key) => ({
-            position: key + 1,
-            name: item.username,
-            score: item.score,
-        }));
-    }
-    return [];
-}
